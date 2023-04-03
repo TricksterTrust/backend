c9d2704cba5570bb6469b31ab562461190a7cdfa
DarkLorianPrime <lolpokens@mail.ru>
2023-04-03 04:05:02 +0400

    hide secret_key

--- backend/app/TrickerTrust/core/settings.py
+++ backend/app/TrickerTrust/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)udzc%mluvst($oj+y*#9@yot2dyxohputai0l2d@%3e0vurao"
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
