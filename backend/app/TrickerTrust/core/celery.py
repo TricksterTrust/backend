57548512324e6506daa6062e09e63d654c8e2c04
DarkLorianPrime <lolpokens@mail.ru>
2023-04-11 00:45:44 +0400

    Fix celery

--- backend/app/TrickerTrust/core/celery.py
+++ backend/app/TrickerTrust/core/celery.py
@@ -1,15 +1,17 @@
 import os
 from os import getenv
 from celery import Celery
-
+from django.conf import settings
 redis_password = getenv("REDIS_PASSWORD")
 redis_host = getenv("REDIS_HOST")
 redis_database = getenv("REDIS_DATABASE")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 redis_url = f"redis://:{redis_password}@{redis_host}/{redis_database}"
 app = Celery("core", broker=redis_url, backend=redis_url)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
 
 app.add_periodic_task(3600 * 3, app.signature("currency_course.tasks.update_course"))
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,5 +3,5 @@ python manage.py makemigrations
 python manage.py migrate
 
 celery -A core.celery.app multi start worker --beat --logfile="$HOME/log/celery/%n%I.log" --pidfile="$HOME/run/celery/%n.pid"
-
+# celery -A core.celery.app worker -B -l debug
 python manage.py runserver 0.0.0.0:8000
