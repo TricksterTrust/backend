a1e41f7cb6b482a946eb3cb10b6dad8ef4b80d4a
DarkLorianPrime <lolpokens@mail.ru>
2023-04-10 23:18:52 +0400

    Update playground

--- backend/app/TrickerTrust/playground_dev/constants.py
+++ backend/app/TrickerTrust/playground_dev/constants.py
@@ -1,28 +1,18 @@
-class Param:
-    QUERY = "query"
-    BODY = "body"
-    PARAM = "param"
-
-
-class Types:
-    STRING = "string"
-
-
 class Result:
     RETRIEVE_DESCRIPTION: str = "Возвращает словарь, в котором params_type выводит всё, что принимает\\выдает эндпоинт, paths содержит все методы и URL для обращения"
+    RETRIEVE_OBJECT: dict = {"params_type": {"description": "Список из всех элементов", "type": "Array[Dict[]]"},
+                             "paths": {"description": "Все методы и эндпоинты", "type": "Array[Dict[]]"}}
     LIST_DESCRIPTION: str = "После успешного выполнения возвращает объект, содержащий словарь с категориями и списками методов"
+    LIST_OBJECT: list = ["Object[str: Array[str]}"]
 
 
 class Params:
     RETRIEVE_DESCRIPTION: str = "Нужный метод для выдачи информации"
-    RETRIEVE_WARNING: str = "Без этой фигни ничего работать не будет!"
 
 
 class Description:
     RETRIEVE_DESCRIPTION: str = "Возвращает всю доступную информацию о переданном методе"
-    RETRIEVE_WARNING: str = "Денис, это временный текст, потом надо убрать"
     LIST_DESCRIPTION: str = "Возвращает словарь содержащий все методы и их категории"
-    LIST_WARNING: str = "Ничего передавать не нужно! Денис, это временный текст, потом надо убрать"
 
 
 class Error:
