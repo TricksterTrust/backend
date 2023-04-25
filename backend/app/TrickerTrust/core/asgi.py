9bb0c8edd9f32f5886afcc7382936a6b1116e974
DarkLorianPrime <lolpokens@mail.ru>
2023-04-25 11:25:28 +0400

    start django as ASGI

--- backend/app/TrickerTrust/core/asgi.py
+++ backend/app/TrickerTrust/core/asgi.py
@@ -8,9 +8,28 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+asgi = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.urls import path
+
+from technical_support.views import ChatConsumer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": asgi,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    path('ws/chat/', ChatConsumer.as_asgi())
+                ]
+            )
+        )
+    )
+})
